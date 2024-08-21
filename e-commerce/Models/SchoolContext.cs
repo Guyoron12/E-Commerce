05cc94bf9fa5fa26cf5ac0c66af848a54f933d9e
orong <orong@GUY>
2024-08-21 22:08:24 +0300

    Add project files.

---
+++ e-commerce/Models/SchoolContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace e_commerce.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Students> students { get; set; }
+        public SchoolContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+    }
+}
---
+++ e-commerce/Models/Students.cs
@@ -0,0 +1,9 @@
+﻿namespace e_commerce.Models
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Enrollmentdate { get; set; }
+    }
+}
